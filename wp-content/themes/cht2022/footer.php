0020e12aaf5bfec8d8d2d893b6acd42d0ee4c21f
Thiago Peixoto <tfpeixoto@gmail.com>
2022-09-12 21:52:24 -0300

    Finalizando o desenvolvimento

--- wp-content/themes/cht2022/footer.php
+++ wp-content/themes/cht2022/footer.php
@@ -48,17 +48,14 @@
   </footer>
 
   <?php
-  require_once('modal.php');
+  require_once('template-parts/modal.php');
   wp_footer();
   ?>
 
-  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/owl.carousel.min.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/scripts.min.js"></script>
-  <script src="<?php bloginfo('template_url'); ?>/js/jquery.mask.min.js"></script>
-  <?php /*<script src="https://use.fontawesome.com/releases/v5.9.0/js/all.js" data-auto-replace-svg="nest"></script>*/ ?>
 
   </body>
 
--- wp-content/themes/cht2022/index.php
+++ wp-content/themes/cht2022/index.php
@@ -1,38 +1,53 @@
-<?php get_header('interna'); ?>
+<?php
+$estiloPagina = 'style.css';
+require_once('header.php');
 
-<main class="mt-5">
-  <section class="container py-5">
-    <div class="row">
-      <div class="col-12 mb-5">
-        <h1 class="font-weight-bold text-vermelho text-center mt-5">Peças novas e usadas</h1>
-      </div>
+$has_category = get_queried_object()->slug;
+$has_category_name = get_queried_object()->name;
+?>
 
-      <div class="card-deck">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        
-        <div class="card">
-          <a href="<?php the_permalink(); ?>">
-            <img src="<?php the_post_thumbnail_url(); ?>" class="card-img-top" alt="<?php the_title(); ?>" />
-          </a>
-
-          <div class="card-body">
-            <h5 class="card-title text-uppercase text-vermelho mb-1">
-              <?php the_category( ', ' ); ?>
-            </h5>
-            <p class="font-weight-bold text-secondary mb-1"><?php the_title(); ?></p>
-            <p class="text-card"><?php the_content(); ?>
-          </div>
-
-          <div class="card-footer text-right border-top-0">
-            <a href="#" role="button" class="btn btn-primary font-weight-bold" data-toggle="modal" data-target="#modalContato">Solicite informações <i class="fas fa-chevron-right"></i></a>
-          </div>
-        </div>          
-
-        <?php endwhile; endif; ?>
-        <?php wp_reset_query(); ?>
-      </div>
+<section class="banner-interno">
+  <div class="row justify-content-center">
+    <div class="col-12">
+      <h1><?= $has_category_name ? $has_category_name : "Casa do Honda e Toyota"; ?></h1>
+      <p class="lead">Peças novas e usadas</p>
     </div>
-  </section>
-</main>
+  </div>
+</section>
+
+<section class="lista">
+  <div class="container">
+    <div class="row justify-content-center">
+      <?php
+      $args = array(
+        'post_type' => 'pecas',
+        'category_name' => $has_category ? $has_category : '',
+        'post_per_page' => 20,
+        'orderby' => 'date'
+      );
+      $pecas = new WP_Query($args);
+
+      if ($pecas->have_posts()) : while ($pecas->have_posts()) : $pecas->the_post(); ?>
+
+          <div class="col-11 col-md-4 lista__post">
+            <?php the_post_thumbnail('thumb-list', array('class' => 'img-fluid')); ?>
+
+            <p class="small"><?php the_category(', '); ?></p>
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_content(); ?></p>
+
+            <a href="#" class="btn" role="button" data-toggle="modal" data-target="#modalContato">Solicite informações</a>
+          </div>
+
+        <?php endwhile;
+      else : ?>
+
+        </p>Não há peças publicadas.</p>
+
+      <?php endif; ?>
+
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
